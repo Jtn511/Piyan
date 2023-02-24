74800f820dfa5985fedd75be8b7107431d984afe
Jtn511 <94739127+Jtn511@users.noreply.github.com>
2023-02-25 00:44:04 +0800

    AA

---
+++ main/DM指令.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import discord, os, json, subprocess
+from discord.ext import commands
+from random import randrange
+from pathlib import Path
+
+intents = discord.Intents.default()
+intents.dm_messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+def load(path):
+    if not path.exists():
+        path.touch()
+    with open(path, 'r', encoding = 'utf8') as f:
+        return json.load(f)
+
+async def announcement(string):
+    #channel = bot.get_channel(1016736583157821453)  # 這裡替換成你的頻道 ID
+    channel = bot.get_channel(1078382554400432242)  # 屁眼
+    await channel.send(string)
+    return
+
+@bot.event
+async def on_message(call):
+    admin = load(Path('C:/Users/jtn91/OneDrive/桌面/Piyan/Data/VIP/admin.json'))
+    if call.guild:  # DM才繼續
+        return
+    if call.author.id in admin: # 權限檢查
+        if '<@1043082295764078652>' in call.content:    # 標記Bot
+            if call.content.split('>')[1].strip() == '公告': # 公告指令
+                r = call.reference.resolved 
+                if r:   # 回覆目標存在
+                    await announcement(r.content)
+                    
+
+token = os.environ.get('DISCORD_BOT_TOKEN')
+
+if token is None:
+    print(token)
+else:
+    bot.run(token)
--- main/刪訊息.py
+++ main/刪訊息.py
@@ -1,5 +1,7 @@
 import discord, os
 from discord.ext import commands
+from pathlib import Path
+import json
 
 intents = discord.Intents.default()
 intents.dm_messages = True
@@ -9,28 +11,36 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 VIP = [634414049815166997] #868565069460545536]
 
+def load(path):
+    if not path.exists():
+        path.touch()
+    with open(path, 'r', encoding = 'utf8') as f:
+        return json.load(f)
+
 @bot.event
 async def on_message(call):
-    if call.content.startswith('ㄇ') and call.author.id in VIP:
-    # 取得指定頻道的訊息
-        if call.reference:
-            await call.reference.resolved.delete()
-            await call.delete()
-        else:
-            try:
-                Guild, Channel, Message = map(int, call.content.split('channels/')[1].split('/'))
+    admin = load(Path('C:/Users/jtn91/OneDrive/桌面/Piyan/Data/VIP/admin.json'))
+    if call.author.id in admin:
+        if call.content.startswith('ㄇ'):
+        # 取得指定頻道的訊息
+            if call.reference:
+                await call.delete()
+                await call.reference.resolved.delete()
+            else:
+                try:
+                    Guild, Channel, Message = map(int, call.content.split('channels/')[1].split('/'))
 
-                channel = bot.get_channel(Channel)  # 這裡替換成你的頻道 ID
-                targetMessage = await channel.fetch_message(Message)  # 這裡替換成你要刪除的訊息 ID
+                    channel = bot.get_channel(Channel)  # 這裡替換成你的頻道 ID
+                    targetMessage = await channel.fetch_message(Message)  # 這裡替換成你要刪除的訊息 ID
 
-                await targetMessage.delete()
-                print(f'{targetMessage.guild}_{targetMessage.channel} | {targetMessage.author}: {targetMessage.content} -> removed')
-                if call.guild:
-                    await call.delete()
-                else:
-                    await call.add_reaction('👌')
-            except:
-                pass
+                    await targetMessage.delete()
+                    print(f'{targetMessage.guild}_{targetMessage.channel} | {targetMessage.author}: {targetMessage.content} -> removed')
+                    if call.guild:
+                        await call.delete()
+                    else:
+                        await call.add_reaction('👌')
+                except:
+                    pass
 
 token = os.environ.get('DISCORD_BOT_TOKEN')
 
